978a187fb44862eb0635b70fa112915aca7bc455
Steve tc chisale <BSC-COM-35-19@unima.ac.mw>
2022-10-05 10:11:29 +0200

    configured the .env file

--- src/main/java/com/spring/security/demo/user/LoginRequest.java
+++ src/main/java/com/spring/security/demo/user/LoginRequest.java
@@ -11,5 +11,5 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginRequest {
-    String userName, password;
+    String userName, password  ;
 }
