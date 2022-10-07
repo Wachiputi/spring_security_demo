31bc25400b67193689ed5caab003ae299df2d544
Steve tc chisale <BSC-COM-35-19@unima.ac.mw>
2022-10-07 09:21:35 +0200

    imported a PreAuthorize framework

--- src/main/java/com/spring/security/demo/user/UserController.java
+++ src/main/java/com/spring/security/demo/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
